c9820eaac9df6431e4654dcf1841618886e24e85
RafaelDVL <rafaelrodrigues.dsg3d@gmail.com>
2023-01-18 13:50:13 -0300

    MainAndNavContent

--- src/app/Shared/material/material.module.ts
+++ src/app/Shared/material/material.module.ts
@@ -3,6 +3,9 @@ import { CommonModule } from '@angular/common';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+
 
 
 
@@ -13,13 +16,17 @@ import {MatButtonModule} from '@angular/material/button';
     CommonModule,
     MatButtonModule,
     MatIconModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule
   ],
   exports: [
     CommonModule,
     MatButtonModule,
     MatIconModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule
   ]
 })
 export class MaterialModule { }
